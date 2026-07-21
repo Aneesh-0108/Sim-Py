e1307fce1f96bdcc73ca6a0d8910bdbe3e4b56a4
Aneesh-0108 <cpaneesh2006@gmail.com>
2026-07-21 22:23:22 +0530

    feat/scan-and-organize

--- multithreader/smart_organizer/scanner.py
+++ multithreader/smart_organizer/scanner.py
@@ -1,6 +1,21 @@
-import os
+
 from pathlib import Path
 
-dir_path = ""
+def validate_and_scan(raw_path:str):
 
-items = os.lis
+#Path-file system object
+
+#expand-user and resolve to clean the path
+    directory = Path(raw_path).expanduser().resolve()
+
+    if not directory.exists():
+        raise FileNotFoundError(f"The path {raw_path} doesnt exist")
+    if not directory.is_dir():
+        raise NotADirectoryError(f"The path {raw_path} is a file,not a diretory")
+    
+    #Iterates through th edirectory and checks for files
+    #interdir() and .is_file()
+
+    files_to_move = [item for item in directory.interdir() if item.is_file()]
+
+    return directory,files_to_move
